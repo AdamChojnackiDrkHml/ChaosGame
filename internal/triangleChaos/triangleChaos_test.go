838969f941b8878aa0fd7090f2786e45cbe726f1
AdamChojnackiDrkHml <72651971+AdamChojnackiDrkHml@users.noreply.github.com>
2022-12-27 11:50:08 +0100

    Update triangleChaos_test.go

--- internal/triangleChaos/triangleChaos_test.go
+++ internal/triangleChaos/triangleChaos_test.go
@@ -14,5 +14,5 @@ func TestCreateChaosTriangle(t *testing.T) {
 
 	bitmap := CreateChaosTriangle(size, stPt, iterations)
 	fmt.Print(bitmap[699*1000+175], bitmap[699*1000+674], bitmap[274*1000+425])
-	assert.True(t, bitmap[699*1000+175] == 0 || bitmap[699*1000+674] == 0 || bitmap[274*1000+425] == 0, "No dupa")
+	assert.True(t, bitmap[699*1000+175] == 0 || bitmap[699*1000+674] == 0 || bitmap[274*1000+425] == 0, "Something went wrong in bitmap setting")
 }
